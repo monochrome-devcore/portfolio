2e1a7c4e22380145cc6e21a0f05fe49881c425ba
Monochrome <monochrome.dev@gmail.com>
2026-06-07 00:13:01 +0700

    update contact and responsive skill
    update projects data

--- src/components/shared/Header.tsx
+++ src/components/shared/Header.tsx
@@ -1,41 +1,44 @@
+// Libs
+import { Link } from 'react-router';
+
 // Assets
-import AvatarImg from "@/assets/avatar.jpg";
+import AvatarImg from '@/assets/avatar.jpg';
 
 // Utils
-import { cn } from "@/lib/utils";
+import { cn } from '@/lib/utils';
 
 const Header = () => {
   return (
-    <header className={cn("mt-10", "max-md:mt-12")}>
+    <header className={cn('mt-10', 'max-md:mt-12')}>
       <div
         className={cn(
-          "container flex gap-10 px-8 py-0",
-          "max-xl:flex-col",
-          "max-md:py-0 max-md:px-4",
+          'container flex gap-10 px-8 py-0',
+          'max-xl:flex-col',
+          'max-md:py-0 max-md:px-4',
         )}
       >
         <div
           className={cn(
-            "flex items-center flex-1 gap-8",
-            "max-xl:flex-col max-xl:items-center",
+            'flex items-center flex-1 gap-8',
+            'max-xl:flex-col max-xl:items-center',
           )}
         >
-          <div className={cn("relative shrink-0")}>
+          <div className={cn('relative shrink-0')}>
             <div
               className={cn(
-                "absolute inset-0 z-0 rounded-full bg-primary animate-voice-ripple",
+                'absolute inset-0 z-0 rounded-full bg-primary animate-voice-ripple',
               )}
             />
 
             <div
               className={cn(
-                "absolute inset-0 rounded-full bg-primary animate-voice-ripple [animation-delay:1000ms] z-0",
+                'absolute inset-0 rounded-full bg-primary animate-voice-ripple [animation-delay:1000ms] z-0',
               )}
             />
 
             <div
               className={cn(
-                "absolute inset-0 rounded-full bg-primary animate-voice-ripple [animation-delay:2000ms] z-0",
+                'absolute inset-0 rounded-full bg-primary animate-voice-ripple [animation-delay:2000ms] z-0',
               )}
             />
 
@@ -43,20 +46,20 @@ const Header = () => {
               src={AvatarImg}
               alt="avatar"
               className={cn(
-                "relative w-[118px] h-[118px] rounded-full shrink-0 object-cover z-1",
+                'relative w-[118px] h-[118px] rounded-full shrink-0 object-cover z-1',
               )}
             />
           </div>
 
-          <div className={cn("flex flex-col gap-2", "max-xl:items-center")}>
-            <h2 className={cn("text-[26px] font-semibold", "max-md:text-2xl")}>
+          <div className={cn('flex flex-col gap-2', 'max-xl:items-center')}>
+            <h2 className={cn('text-[26px] font-semibold', 'max-md:text-2xl')}>
               Monochrome
             </h2>
 
-            <div className={cn("flex gap-1.5 text-center", "max-md:flex-col")}>
+            <div className={cn('flex gap-1.5 text-center', 'max-md:flex-col')}>
               <div
                 className={cn(
-                  "px-3 py-1 text-sm rounded-[4px] text-blue-950 bg-blue-50",
+                  'px-3 py-1 text-sm rounded-[4px] text-blue-950 bg-blue-50',
                 )}
               >
                 Lập trình website
@@ -64,7 +67,7 @@ const Header = () => {
 
               <div
                 className={cn(
-                  "px-3 py-1 text-sm rounded-[4px] text-blue-950 bg-blue-50 line-clamp-1",
+                  'px-3 py-1 text-sm rounded-[4px] text-blue-950 bg-blue-50 line-clamp-1',
                 )}
               >
                 Frontend developer
@@ -73,14 +76,14 @@ const Header = () => {
 
             <p
               className={cn(
-                "text-neutral-600",
-                "max-xl:text-center max-xl:max-w-screen-xs",
+                'text-neutral-600',
+                'max-xl:text-center max-xl:max-w-screen-xs',
               )}
             >
-              Mong muốn ứng tuyển vị trí{" "}
-              <span className={cn("font-semibold text-blue-950")}>
+              Mong muốn ứng tuyển vị trí{' '}
+              <span className={cn('font-semibold text-blue-950')}>
                 Junior Developer
-              </span>{" "}
+              </span>{' '}
               trong lĩnh vực phát triển website.
             </p>
           </div>
@@ -88,25 +91,31 @@ const Header = () => {
 
         <div
           className={cn(
-            "flex flex-col items-end flex-1 gap-2",
-            "max-xl:items-center max-xl:text-center",
+            'flex flex-col items-end flex-1 gap-2',
+            'max-xl:items-center max-xl:text-center',
           )}
         >
-          <h2 className={cn("text-[26px] font-semibold", "max-md:text-2xl")}>
+          <h2 className={cn('text-[26px] font-semibold', 'max-md:text-2xl')}>
             Thông Tin Liên Hệ
           </h2>
 
-          <p className={cn("text-neutral-600")}>
-            Gửi email cho mình tại:{" "}
-            <span className={cn("font-semibold text-blue-950")}>
+          <p className={cn('text-neutral-600')}>
+            Gửi email cho mình tại:{' '}
+            <span className={cn('font-semibold text-blue-950')}>
               monochrome.dev@gmail.com
             </span>
           </p>
 
-          <p className={cn("text-neutral-600")}>
-            Gọi cho mình tại:{" "}
-            <span className={cn("font-semibold text-blue-950")}>
-              0912.678.543
+          <p className={cn('text-neutral-600')}>
+            Github của mình:{' '}
+            <span className={cn('font-semibold text-blue-950')}>
+              <Link
+                to={'https://github.com/monochrome-devcore'}
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                github.com/monochrome-devcore
+              </Link>
             </span>
           </p>
         </div>
--- src/data/projectData.ts
+++ src/data/projectData.ts
@@ -1,10 +1,10 @@
 // Assets
-import Project1Img from "@/assets/project1.jpg";
-import Project2Img from "@/assets/project2.jpg";
-import Project3Img from "@/assets/project3.jpg";
-import Project4Img from "@/assets/project4.jpg";
-import Project5Img from "@/assets/project5.jpg";
-import Project6Img from "@/assets/project6.jpg";
+import Project1Img from '@/assets/project-portfolio.png';
+import Project2Img from '@/assets/project-mini-apt.png';
+import Project3Img from '@/assets/project-weather.png';
+// import Project4Img from '@/assets/project4.jpg';
+// import Project5Img from '@/assets/project5.jpg';
+// import Project6Img from '@/assets/project6.jpg';
 
 interface ProjectData {
   id: string;
@@ -20,61 +20,61 @@ export const mockProjectData: ProjectData[] = [
   {
     id: crypto.randomUUID(),
     image: Project1Img,
-    title: "Todo List",
+    title: 'Portfolio',
     description:
-      "Ghi chú nhanh chóng, tiện lơi. Giao diện dễ sử dụng, thân thiện với người dùng.",
-    developBy: "Monochrome",
-    githubUrl: "https://github.com/",
-    demoUrl: "",
+      'Dự án thiết kế và phát triển trang web cá nhân đóng vai trò như một bản CV trực quan sinh động nhất. Nơi tổng hợp toàn bộ thông tin, kỹ năng chuyên môn và các cột mốc ấn tượng trong quá trình làm nghề của bản thân.',
+    developBy: 'Monochrome',
+    githubUrl: '',
+    demoUrl: 'https://portfolio-wine-alpha-70.vercel.app/',
   },
   {
     id: crypto.randomUUID(),
     image: Project2Img,
-    title: "Trang Thông Tin Công Nghệ",
+    title: 'Mini Apartment Management',
     description:
-      "Cập nhật tin tức mới nhất về công nghệ. Theo dõi tình hình công nghệ trên toàn thế giới.",
-    developBy: "Monochrome",
-    githubUrl: "https://github.com/",
-    demoUrl: "",
+      'Hệ thống dashboard thông minh giúp tối ưu hóa việc quản lý vận hành chung cư mini từ doanh thu, hợp đồng đến dịch vụ điện nước. Giao diện trực quan cung cấp số liệu phân tích thời gian thực, giúp chủ nhà và quản lý đưa ra quyết định chính xác, nhanh chóng.',
+    developBy: 'Monochrome',
+    githubUrl: 'https://github.com/quandevfr/mini-apt-frontend',
+    demoUrl: '',
   },
   {
     id: crypto.randomUUID(),
     image: Project3Img,
-    title: "Spotify Clone",
+    title: 'Weather App',
     description:
-      "Nghe nhạc, xem video hoàn toàn miễn phí. Nghe những album yêu thích ngay cả khi không có kết nối mạng.",
-    developBy: "Monochrome",
-    githubUrl: "https://github.com/",
-    demoUrl: "",
+      'Ứng dụng cung cấp thông tin dự báo thời tiết theo thời gian thực với độ chính xác cao về nhiệt độ, độ ẩm và tốc độ gió.',
+    developBy: 'Monochrome',
+    githubUrl: '',
+    demoUrl: 'https://weather-app-one-beta-12.vercel.app/',
   },
-  {
-    id: crypto.randomUUID(),
-    image: Project4Img,
-    title: "Facebook Clone",
-    description:
-      "Kết nối, trò chuyện với bạn bè ở khắp nơi. Chia sẻ những khoảnh khắc thú vị trong cuộc sống.",
-    developBy: "Monochrome",
-    githubUrl: "https://github.com/",
-    demoUrl: "",
-  },
-  {
-    id: crypto.randomUUID(),
-    image: Project5Img,
-    title: "Quản Lý Doanh Nghiệp",
-    description:
-      "Quản lý nhân viên, theo dõi chấm công, tự động tính toán bảng lương cho nhân viên.",
-    developBy: "Monochrome",
-    githubUrl: "https://github.com/",
-    demoUrl: "",
-  },
-  {
-    id: crypto.randomUUID(),
-    image: Project6Img,
-    title: "Portfolio",
-    description:
-      "Trang giới thiệu online, giúp mọi người có thể tìm kiếm, xem thông tin và liên hệ dễ dàng.",
-    developBy: "Monochrome",
-    githubUrl: "https://github.com/",
-    demoUrl: "",
-  },
+  // {
+  //   id: crypto.randomUUID(),
+  //   image: Project4Img,
+  //   title: 'Facebook Clone',
+  //   description:
+  //     'Kết nối, trò chuyện với bạn bè ở khắp nơi. Chia sẻ những khoảnh khắc thú vị trong cuộc sống.',
+  //   developBy: 'Monochrome',
+  //   githubUrl: 'https://github.com/',
+  //   demoUrl: '',
+  // },
+  // {
+  //   id: crypto.randomUUID(),
+  //   image: Project5Img,
+  //   title: 'Quản Lý Doanh Nghiệp',
+  //   description:
+  //     'Quản lý nhân viên, theo dõi chấm công, tự động tính toán bảng lương cho nhân viên.',
+  //   developBy: 'Monochrome',
+  //   githubUrl: 'https://github.com/',
+  //   demoUrl: '',
+  // },
+  // {
+  //   id: crypto.randomUUID(),
+  //   image: Project6Img,
+  //   title: 'Portfolio',
+  //   description:
+  //     'Trang giới thiệu online, giúp mọi người có thể tìm kiếm, xem thông tin và liên hệ dễ dàng.',
+  //   developBy: 'Monochrome',
+  //   githubUrl: 'https://github.com/',
+  //   demoUrl: '',
+  // },
 ];
