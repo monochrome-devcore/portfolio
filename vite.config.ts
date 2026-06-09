5361d9a63a233c43a646563095cca6c29796147e
Monochrome <monochrome.dev@gmail.com>
2026-06-09 16:44:32 +0700

    update config lazy image

--- vite.config.ts
+++ vite.config.ts
@@ -1,16 +1,19 @@
-import path from "path";
-import tailwindcss from "@tailwindcss/vite";
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+import path from 'path';
+import tailwindcss from '@tailwindcss/vite';
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
-      "@": path.resolve(__dirname, "./src"),
-      "@/components": path.resolve(__dirname, "./src/components"),
-      "@/assets": path.resolve(__dirname, "./src/assets"),
+      '@': path.resolve(__dirname, './src'),
+      '@/components': path.resolve(__dirname, './src/components'),
+      '@/assets': path.resolve(__dirname, './src/assets'),
     },
+  },
+  build: {
+    assetsInlineLimit: 10240,
   },
 });
